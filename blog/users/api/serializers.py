6e45103277eb8b605b98d33eda731e75ad76b03e
Luciano Labudia <labudia.luciano@gmail.com>
2022-12-30 12:02:32 -0300

    Encrypting password in the registry

--- blog/users/api/serializers.py
+++ blog/users/api/serializers.py
@@ -2,6 +2,17 @@ from rest_framework import serializers
 from users.models import User
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'password']
+
+    
+    def create(self, validate_data):
+        password = validate_data.pop('password', None)
+        instance = self.Meta.model(**validate_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
