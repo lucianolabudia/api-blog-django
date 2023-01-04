3a2f81a53f4c60df9c6811a9893e01e3bf1efd71
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-04 16:54:17 -0300

    Creating the CRUD of the posts

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,6 +19,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from categories.api.router import router_categories
+from posts.api.router import router_posts
 
 
 schema_view = get_schema_view(
@@ -41,5 +42,6 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('users.api.router')),
-    path('api/', include(router_categories.urls))
+    path('api/', include(router_categories.urls)),
+    path('api/', include(router_posts.urls)),
 ]
