c822e9375730dc0b13e6ac1a3fa56cd75fa9088e
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-04 10:29:15 -0300

    Filtering with django_filter and internal filters

--- blog/categories/api/views.py
+++ blog/categories/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 from categories.models import Category
 from categories.api.serializer import CategorySerializer
 from categories.api.permissions import IsAdminOrReadOnly
@@ -8,4 +10,7 @@ class CategoryApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    # queryset = Category.objects.filter(published=True)
     lookup_field = 'slug'
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['published', 'title']
