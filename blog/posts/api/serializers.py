b0b79e7724b44eaa408827231e9ce9b2efaee86a
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-06 15:22:16 -0300

    Getting all the user information from the post

--- blog/posts/api/serializers.py
+++ blog/posts/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from posts.models import Post
+from users.api.serializers import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer()
 
     class Meta:
         model = Post
