c1e4ab0f933c417063ea29f7aab3f3133f4ece81
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-06 15:25:07 -0300

    Getting all the information from the post category

--- blog/posts/api/serializers.py
+++ blog/posts/api/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from posts.models import Post
 from users.api.serializers import UserSerializer
+from categories.api.serializer import CategorySerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
 
     user = UserSerializer()
+    category = CategorySerializer()
 
     class Meta:
         model = Post
