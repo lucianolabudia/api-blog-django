99f84af39d3e8f6b2e1a141659e39432ebe3982e
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-06 17:22:20 -0300

    Getting the comments on a post

--- blog/comments/api/views.py
+++ blog/comments/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from comments.models import Comment
 from comments.api.serializers import CommentSerializer
@@ -9,5 +10,6 @@ class CommentApiViewSet(ModelViewSet):
 
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
-    filter_backends = [OrderingFilter]
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
     ordering = ['-created_at']
+    filterset_fields = ['post']
