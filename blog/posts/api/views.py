f4fc16e678af874909e4a82ed06148ef7547fe73
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-06 15:39:02 -0300

    Get all posts in a category

--- blog/posts/api/views.py
+++ blog/posts/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 from posts.models import Post
 from posts.api.serializers import PostSerializer
 from posts.api.permissions import IsAdminOrReadOnly
@@ -9,3 +11,6 @@ class PostApiViewSet(ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(published=True)
     lookup_field = 'slug'
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'category__slug']
+    # filterset_fields = ['category']
