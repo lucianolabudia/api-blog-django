e51e73501569fd1d3aad74d6d5484c96b38c380b
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-06 17:43:42 -0300

    Comment permissions system

--- blog/comments/api/views.py
+++ blog/comments/api/views.py
@@ -4,10 +4,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from comments.models import Comment
 from comments.api.serializers import CommentSerializer
+from comments.api.permissions import IsOwnerOrReadAndCreateOnly
 
 
 class CommentApiViewSet(ModelViewSet):
 
+    permission_classes = [IsOwnerOrReadAndCreateOnly]
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
     filter_backends = [OrderingFilter, DjangoFilterBackend]
