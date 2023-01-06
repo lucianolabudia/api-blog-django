307e95c8798ee84164070ec6390eb0a4fb2f2ee0
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-06 17:15:22 -0300

    Order system

--- blog/comments/api/views.py
+++ blog/comments/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
 
 from comments.models import Comment
 from comments.api.serializers import CommentSerializer
@@ -8,3 +9,5 @@ class CommentApiViewSet(ModelViewSet):
 
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    filter_backends = [OrderingFilter]
+    ordering = ['-created_at']
